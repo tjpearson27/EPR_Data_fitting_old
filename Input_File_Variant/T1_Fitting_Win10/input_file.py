860239fb06063245c7c016fb1dd117d9d981a520
Tyler Pearson <tjpearson27@gmail.com>
2019-04-10 16:24:48 -0500

    created new fields in input file

--- Input_File_Variant/T1_Fitting_Win10/input_file.py
+++ Input_File_Variant/T1_Fitting_Win10/input_file.py
@@ -9,21 +9,27 @@ b = 0
 T1 = 310000000
 mono_bnds = [(), ()]
 
+###########################################################################################
 #only for spec_diffus (spectral diffusion parameter)
 q = 1000000
+#sd_bnds = [(a_lower, T1_lower, q_lower, b_lower), (upper bounds same order)]
 sd_bnds = [(0.7, 100, 10, -0.2), (1.2, 1000000000000, 100000000, 0.3)]
 
-
+###########################################################################################
 #only for stretched (stretch factor)
 c = 0.5
+#str_bnds = [(b_lower, a1_lower, T1_lower, c_lower), (upper bounds same order)]
 str_bnds = [(), ()]
 
+###########################################################################################
 #params for biexp
 a2 = 0.5
 T1long = 30000000
 T1short = 10000
+#biexp_bnds = [(b_lower, a1_lower, T1_lower, c_lower), (upper bounds same order)]
 biexp_bnds = [(), ()]
 
+###########################################################################################
 #folder to spit data out into
 folder_name = 'T1_fit_{0}'.format(function)
 
